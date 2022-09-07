4a0052ecd0a16608419fbdcbef5071934d615ff7
Jordan Maynor <JMaynor@pepsimidamerica.com>
2022-09-07 13:26:33 -0500

    removed unused functions

--- dolores.py
+++ dolores.py
@@ -5,8 +5,8 @@ Date: Apr 2020
 
 Dolores is a chatbot that connects to a Discord server. Her primary use
 is in being able to roll dice for players of a tabletop roleplaying game
-but she is also capable of keeping track of player's inventories and "talking"
-to the players through a few different commands.
+but she is also capable of doing some basic audio things as well as creating
+images using Stable Diffusion.
 '''
 
 # pylint: disable=line-too-long
@@ -58,7 +58,9 @@ sarcastic_names = ['my lovely',
 				   'dearest',
 				   'lover',
 				   'honey',
-				   'foxy mama']
+				   'foxy mama',
+				   'loathsome dung eater',
+				   'baby girl']
 
 bot = commands.Bot(command_prefix='-', case_insensitive=True)
 
@@ -67,15 +69,7 @@ config_file = '/home/dolores/config/config.yml'
 with open(config_file) as c:
 	config = yaml.load(c)
 bot_api_key = config['DISCORD']['bot_api_key']
-general_chat_id = config['DISCORD']['general_chat_id']
-general_voice_id = config['DISCORD']['general_voice_id']
-log_rolls = config['DATABASE']['log_rolls']
-admins = config['DISCORD']['admin_users']
-inv_admins = config['DISCORD']['inv_admin_users']
 diffusion_access_token = config['DISCORD']['diffusion_key']
-
-# In-container location, use volume for local file storage
-db_loc = 'file:/home/dolores/config/roll_history.db?mode=rw'
 
 yt_dlp.utils.bug_reports_message = lambda: ''
 ffmpeg_options = {'options': '-vn'}
@@ -115,7 +109,9 @@ async def on_command_error(ctx, error):
 			'Must I hold your hand for this?',
 			'Oh, y\'all still can\'t type?',
 			'Girl, go hit up Mavis Beacon, cuz you cannot type.',
-			'Close.']
+			'Close.',
+			'Slow.',
+			'Homeless.']
 		await ctx.send(random.choice(snarky_comments))
 	else:
 		print(error, file=sys.stderr)
@@ -147,66 +143,6 @@ async def send_result(ctx, message):
 		await ctx.send('{}:    {}'.format(str(ctx.author.nick).split('#')[0], message))
 
 
-@bot.command(description='Sets nickname of everyone to whichever campaign is now active.')
-async def setcamp(ctx, campaign: str):
-	'''
-	setcamp sets everyone's nicknames to the character
-	they play in whichever campaign is specified. Command
-	only usable by server owner.
-	Ex: -setcamp ternov
-	Dolores would set everyone's names to whatever character they play in the "ternov" campaign
-	'''
-	if str(ctx.author).split('#')[0] in admins:
-		# Contains everyone's names for each campaign that has been run
-		chars = pandas.DataFrame(data=config['CHARACTERS'], index=config['USERS'])
-		for user in ctx.guild.members:
-			try:
-				newnick = chars.loc[user.name, campaign]
-				print('New nickname for {} is {} in campaign {}'.format(user.name, newnick, campaign) )
-				if newnick is not None:
-					await user.edit(nick=newnick)
-			except KeyError: print('No value found for user/camp combo: {} {}'.format(user, campaign))
-			except discord.Forbidden: print('Couldn\'t change the nickname of user: {}'.format(user))
-		await send_result(ctx, 'I have performed the ritual of renaming.')
-	else: await send_result(ctx, 'You ain\'t got the authority to do that, sir or madam.')
-
-@bot.command(description='Reloads config file so any changes to characters section can be used without restarting Dolores')
-async def reload_config(ctx):
-	if str(ctx.author).split('#')[0] in admins:
-		with open(config_file) as c:
-			global config
-			config = yaml.load(c)
-		await send_result(ctx, 'I have reloaded my configuration file.')
-
-
-@bot.command(description='Command for transferring DM power to a user.')
-async def dm(ctx, member: discord.Member):
-	'''
-	dm transfers DM powers to a given user.
-	It first removes the DM user from any users that already have it.
-	Should only ever be one user, but might as well loop through all.
-	Ex: -dm Makamatin
-	Dolores would remove DM role from all users, then would assign role to user Makamatin
-	'''
-	if str(ctx.author).split('#')[0] in admins:
-		dm_role = discord.utils.get(ctx.guild.roles, name='DM')
-		for user in ctx.guild.members:
-			await bot.remove_roles(user, dm_role)
-			if member in str(user.name):
-				await bot.add_roles(user, dm_role, reason='Campaign switch.')
-		await send_result(ctx, 'The Ritual has been performed.')
-	else: await send_result(ctx, 'Only the Old King of this land may wield such power.')
-
-@bot.command(description='Sets Dolores\' status in the Discord server sidebar.')
-async def set_status(ctx, game: str):
-	'''
-	set_status sets the Dolores is playing () status on the sidebar to whatever is specified.
-	Ex: -set_status "Destroying the Patriarchy"
-	The current activity on Dolores user would show as Dolores is playing Destroying the Patriarchy
-	'''
-	await bot.change_presence(status=discord.Status.online, activity=discord.Game(game))
-
-
 #---------------------------------------------------------------------------
 # Dice Rolling & Randomization
 #---------------------------------------------------------------------------
@@ -228,11 +164,7 @@ async def roll(ctx, *dice_batches):
 		if len(rolls_result) > 500:
 			await ctx.send(random.choice(['I ain\'t rollin all that for you...', 'Absolutely not.', 'No.']))
 			return
-		if len(rolls_result) < 100 and log_rolls:
-			for r in rolls_result: log_roll(ctx, limit, r)
-		formatted_rolls = ', '.join(rolls_result)
-		formatted_rolls = '(d' + str(limit) + ')  ' + formatted_rolls
-		# if len(rolls_result) >= 3:
+		formatted_rolls = '(d' + str(limit) + ')  ' + ', '.join(rolls_result)
 		if limit != 20 and len(rolls_result) >= 1:
 			formatted_rolls = formatted_rolls + '    Sum: ' + \
 				str(sum([int(x) for x in rolls_result]))
@@ -257,121 +189,22 @@ async def d20(ctx):
 	Dolores rolls a single d20 and returns the result.
 	'''
 	roll_result = random.randint(1, 20)
-	if log_rolls:
-		log_roll(ctx, '20', roll_result)
 	roll_result = '(d20)  ' + str(roll_result)
 	await send_result(ctx, roll_result)
 
 
-def log_roll(ctx, dtype, roll):
-	'''
-	Logs a die roll someone does into a sqlite database for later perusing.
-	'''
-	row = [str(ctx.author).split('#')[0], dtype, roll]
-	try:
-		c.execute("INSERT INTO rolls VALUES (?,?,?, DATETIME('now', 'localtime'))", row)
-		conn.commit()
-	except:
-		print('ERROR INSERTING ROLL INTO rolls SQLITE TABLE.')
-
-
-#---------------------------------------------------------------------------
-# Inventory System
-#---------------------------------------------------------------------------
-
-@bot.command(description='Adds or removes an item to someone\'s inventory.')
-# @commands.has_any_role('Admin', 'Inventory Admin')
-async def inv(ctx, person: str, item: str, count=1):
-	'''
-	Add/remove item to specified person\'s inventory. Positive num for add, negative for subtract
-	Ex: -inv Dieter "Cursed Book" 1
-	Dolores will add 1 cursed book to Dieter's inventory
-	'''
-	print('Person ', person, ' Item ', item, ' Count ', count)
-
-	if str(ctx.author).split('#')[0] in [*inv_admins, *admins]:
-		amount_person_has = get_item_amount(person, item)
-		if amount_person_has + count <= 0:
-			c.execute("DELETE FROM inventory WHERE person = ? and item = ?", [person, item])
-			conn.commit()
-			await send_result(ctx, 'Character has no more of that item now.')
-		elif count < 0:
-			if amount_person_has is None:
-				await send_result(ctx, 'Person does not have that item in inventory.')
-			else:
-				try:
-					c.execute("UPDATE inventory SET count=count-? WHERE person = ? and item = ?", [abs(count), person, item])
-					conn.commit()
-					await send_result(ctx, 'Edited that item count.')
-				except:
-					await send_result(ctx, 'ERROR SUBSTRACTING THAT ITEM')
-		else:
-			try:
-				c.execute("INSERT INTO inventory(person,count,item) VALUES (?,?,?) ON CONFLICT(person,item) DO UPDATE SET count=count+? WHERE person = ? AND item = ?",
-						[person, count, item, count, person, item])
-				conn.commit()
-				await send_result(ctx, 'Edited that item count.')
-			except:
-				await send_result(ctx, 'ERROR ADDING ITEM')
-
-
-@bot.command(description='Show a character\'s inventory.')
-async def invshow(ctx, person=None):
-	'''
-	Shows a person\'s inventory. Person can be specified, if not it will show inventory of person who called function.
-	Ex: -invshow Legion
-	Legion's inventory will be displayed.
-	'''
-	if person is None: person = get_character(str(ctx.author).split('#')[0])
-	try:
-		print('Returning inventory for: ', person)
-		c.execute("SELECT count, item from inventory WHERE person = ? ORDER BY item", [person])
-		text_result = '```\n'
-		for row in c.fetchall():
-			text_result = text_result + str(row[0]) + '	' + str(row[1]) + '\n'
-		text_result = text_result + '```'
-		await send_result(ctx, text_result)
-	except: await send_result(ctx, 'ERROR SELECTING FROM THE inventory TABLE.')
-
-
-def get_item_amount(person, item):
-	'''
-	Utility function for fetching current amount of item a person has
-	Will return None if character does not have an item at all.
-	'''
-	c.execute("SELECT count FROM inventory WHERE person = ? AND item = ?", [person, item])
-	result = c.fetchone()
-	return 0 if result is None else result[0]
-
-
-@bot.command(description='Set the active campaign for the inventory commands.')
-async def set_character(ctx, user, character):
-	'''
-	set_campaign sets the currently active campaign to the given ID
-	This is for use with the inventory system, it allows each user to
-	have a character in each campaign
-	SYSTEM NOT CURRENTLY BEING USED
-	'''
-	if str(ctx.author).split('#')[0] in [*inv_admins, *admins]:
-		try:
-			c.execute("UPDATE user_character SET character=? WHERE user=?", [character, user])
-			await send_result(ctx, 'Set user\'s active character.')
-		except: await send_result(ctx, 'ERROR SETTING ACTIVE CAMPAIGN')
-
-
-def get_character(user):
-	'''
-	Utility function for returning the ID of the currently active campaign
-	'''
-	return c.execute("SELECT character from user_character WHERE user=?", [user]).fetchone()[0]
-
 #---------------------------------------------------------------------------
 # Image Creation
 #---------------------------------------------------------------------------
+
 @bot.command(description='Creates an image from the word prompt.')
 async def create(ctx, *, prompt):
 	image = pipe(prompt)["sample"][0]
-	image.save("diffuse{0}.png".format(datetime.now().strftime("%Y-%m-%d %H%M%S")))
+	image_name = "{0}{1}.png".format(str(ctx.author).split('#')[0], datetime.now().strftime("%Y-%m-%d %H%M%S"))
+	image.save(image_name)
+	image_file = discord.File(image_name)
+	ctx.reply(file=image_file, content='This is my design.')
+
 
 #---------------------------------------------------------------------------
 # Audio/Music
@@ -409,31 +242,32 @@ async def speak(ctx, message=None):
 	Ex: -speak
 	Dolores will randomly say a phrase from a predetermined list.
 	'''
-	if str(ctx.author).split('#')[0] in admins:
-		channel = bot.get_channel(general_chat_id)
-		async with channel.typing():
-			await asyncio.sleep(1)
-		await channel.send(message)
-	else: await ctx.send(random.choice(list_of_phrases), tts=True)
+	await send_result(ctx, random.choice(list_of_phrases), tts=True)
 
 
 @bot.command(description='Use\'s youtube-dl to play an audio stream in the General voice channel.')
 async def play(ctx, *, url):
 	'''
-	Plays a song from a given URL in the General voice channel.
+	Plays a song from a given URL in the user's current voice channel.
 	Valid URLS are Youtube and Soundcloud
 	Ex: -play https://www.youtube.com/watch?v=O1OTWCd40bc
 	Dolores will play Wicked Games by The Weeknd
-	Note: Due to drama with youtube-dl, may or may mot be working at this point
+	Note: Due to drama with yt-dlp, may or may mot be working at this point
 	'''
-	if ctx.voice_client is None: await bot.get_channel(general_voice_id).connect()
+	member = ctx.message.author
+	member_id = member.id
+	try:
+		channel = member.voice.channel
+		if channel:
+			await channel.connect()
+	except AttributeError: await send_result(ctx, 'Must be connected to voice channel to play audio.')
+
 	if ctx.voice_client.is_playing(): ctx.voice_client.stop()
 
 	async with ctx.typing():
 		player = await YTDLSource.from_url(url, loop=bot.loop, stream=False)
-		ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+		channel.play(player, after=lambda e: print('Player error: {}'.format(e)) if e else None)
 	await ctx.send('Now playing: {}'.format(player.title))
-
 
 @bot.command(description='Stops the currently playing audio in the General voice channel.')
 async def stop(ctx):
@@ -460,11 +294,4 @@ async def leave(ctx):
 #---------------------------------------------------------------------------
 if __name__ == '__main__':
 	print('Starting main program...')
-	try:
-		conn = sqlite3.connect(db_loc, uri=True)
-		c = conn.cursor()
-	except sqlite3.OperationalError:
-		print('DATABASE NOT FOUND')
-		exit()
-
 	bot.run(bot_api_key)
