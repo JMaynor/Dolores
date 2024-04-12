7530162a6de5d7ec08612b03efd6c85668fa370d
Jordan Maynor <mail@jtm.li>
2024-04-12 16:45:09 -0500

    Adding node setup

--- modules/audio.py
+++ modules/audio.py
@@ -6,39 +6,9 @@ import asyncio
 
 import discord
 import pomice
-from configload import config
 from discord.ext import bridge, commands
 
-# yt_dlp.utils.bug_reports_message = lambda: ""
-# ffmpeg_options = {"options": "-vn"}
-# ytdl = yt_dlp.YoutubeDL(config["YTDL"])
-
-
-class YTDLSource(discord.PCMVolumeTransformer):
-    """
-    The YTDLSource class represents an individual source of music.
-    """
-
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-        self.data = data
-        self.title = data.get("title")
-        self.url = data.get("url")
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        """
-        from_url pulls in the actual audio data from a given URL
-        """
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(
-            None, lambda: ytdl.extract_info(url, download=not stream)
-        )
-        if "entries" in data:
-            # take first item from a playlist
-            data = data["entries"][0]
-        filename = data["url"] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+from configload import config
 
 
 class audio(commands.Cog):
@@ -48,6 +18,17 @@ class audio(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.pomice = pomice.NodePool()
+        self.queue = pomice.Queue()
+
+    async def start_nodes(self):
+        await self.pomice.create_node(
+            bot=self.bot,
+            host=config["LAVALINK"]["host"],
+            port=config["LAVALINK"]["port"],
+            password=config["LAVALINK"]["password"],
+            identifier="Dolores",
+        )
 
     @bridge.bridge_command(
         description="Use's yt-dlp to play an audio stream in the user's voice channel."
